0051b68a3d7c3c32b8fe0f86cf64dc2b0e844a06
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-05-18 21:30:45 +0200

    fix simka

--- metagenomix/commands.py
+++ metagenomix/commands.py
@@ -175,7 +175,6 @@ class Commands(object):
         self.out = out_dir
 
     def prep_simka(self):
-        print(self.inputs)
         inp = get_simka_input(self.dir, self.inputs)
         self.soft.io['I']['f'].add(inp)
         smin = True
