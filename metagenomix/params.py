cbc03c164a3514563efa1199d0b81d344ad53eac
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-05-28 20:22:00 +0200

    split errors in params filling and checking

--- metagenomix/params.py
+++ metagenomix/params.py
@@ -9,25 +9,46 @@
 from os.path import isdir, isfile
 
 
+def check_ints(param, value, soft):
+    if not str(value).isdigit():
+        raise IOError('Param "%s" for "%s" must be integer' % (
+            param, soft.name))
+
+
+def check_mems(param, value, soft):
+    mems = ['kb', 'mb', 'gb']
+    if value not in mems:
+        raise IOError('Param "%s" for "%s" must be of %s' % (
+            param, soft.name, str(mems)))
+
+
+def check_env(config, value, soft):
+    if soft.name not in config.modules and value not in config.conda_envs:
+        raise EnvironmentError('"%s" not a conda env' % value)
+
+
+def check_path(value):
+    if not isfile(value) and not isdir(value):
+        raise IOError('"%s" do not exist' % value)
+
+
+def check_soft_params():
+    pass
+
+
 def set_user_params(config, soft):
-    mem_dim = ['kb', 'mb', 'gb']
-    integer_params = ['time', 'procs', 'mem_num', 'chunks']
+    ints = ['time', 'procs', 'mem_num', 'chunks']
     soft_params = config.user_params.get(soft.name, {})
     for param, value in soft_params.items():
-        print(param, value)
         soft.params[param] = value
-        if param in integer_params:
-            if not str(value).isdigit():
-                raise IOError('Param "%s" for "%s" must be integer' % (
-                    param, soft.name))
+        if param in ints:
+            check_ints(param, value, soft)
         elif param == 'mem_dim':
-            if value not in mem_dim:
-                raise IOError('Param "%s" for "%s" must be of %s' % (
-                    param, soft.name, str(mem_dim)))
+            check_mems(param, value, soft)
         elif param == 'env':
-            if value in config.conda_envs:
-                raise EnvironmentError('"%s" not a conda env' % value)
+            check_env(config, value, soft)
         elif param == 'path':
-            if not isfile(value) and not isdir(value):
-                raise IOError('"%s" do not exist' % value)
+            check_path(value)
+        else:
+            check_soft_params()
         soft.params[param] = value
