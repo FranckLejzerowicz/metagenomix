4a4d3726c8019e8e593e4fc56bac74fa9e435eea
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-07-04 00:34:48 +0200

    move fastq edit and count to preprocess.py, refine pfam functions, finalize scratching and make a function to handle io

--- metagenomix/_io_utils.py
+++ metagenomix/_io_utils.py
@@ -8,9 +8,8 @@
 
 import os
 import re
-import gzip
+import sys
 import yaml
-import subprocess
 import pandas as pd
 from os.path import abspath, basename, dirname, isfile
 
@@ -78,125 +77,6 @@ def fill_fastq(fastq_paths: list, sams: set):
     return fastq
 
 
-def get_fastq_header(
-        fastq_path: str
-) -> str:
-    """Get the first line of the fastq file.
-
-    Parameters
-    ----------
-    fastq_path : str
-        Path to a fastq.gz or fastq file.
-
-    Returns
-    -------
-    fastq_line : str
-        First line of the fastq file.
-    """
-    fastq_line = ''
-    if fastq_path.endswith('fastq.gz'):
-        with gzip.open(fastq_path) as f:
-            for line in f:
-                fastq_line = line.decode().strip()
-                break
-    elif fastq_path.endswith('fastq'):
-        with open(fastq_path) as f:
-            for line in f:
-                fastq_line = line.strip()
-                break
-    return fastq_line
-
-
-def get_cat_zcat(
-        fastq_fp: str
-) -> str:
-    """Get the command `cat` or `gzcat` to run for fastq editing.
-
-    Parameters
-    ----------
-    fastq_fp : str
-        Fastq file path.
-
-    Returns
-    -------
-    cat : str
-        The command to use (`cat` or `zcat`).
-    """
-    cat = "cat"
-    if fastq_fp.endswith('.fastq.gz'):
-        cat = "zcat"
-    return cat
-
-
-def edit_fastq_cmd(
-        fastq_fp: str,
-        num: int,
-        source: str
-) -> str:
-    """Get the unix command to run on each fastq file that
-    needs editing, depending on the source of fastq file.
-
-    Parameters
-    ----------
-    fastq_fp : str
-        Fastq file path.
-    num : int
-        Can be 1 or 2 for the forward and reverse read, respectively.
-    source : str
-        Source of the fastq that defines format and editing type.
-
-    Returns
-    -------
-    cmd : str
-        Unix command to run to edit a fastq file.
-    """
-    cat = get_cat_zcat(fastq_fp)
-    cmd = '%s %s | ' % (cat, fastq_fp)
-    cmd += "awk '{ if (NR%s4==1) " % '%'
-    if source == 'illumina':
-        cmd += "{ print $1\"/%s\" } " % str(num)
-    elif source == 'ebi':
-        cmd += "{ gsub(/ .*/,\"/%s\",$0); print } " % num
-    cmd += "else if (NR%s2 == 1) { print \"+\" } " % '%'
-    cmd += "else { print } }' | gzip > %s_renamed\n" % fastq_fp
-    cmd += "mv %s_renamed %s" % (fastq_fp, fastq_fp)
-    if fastq_fp.endswith('.fastq'):
-        cmd += ".gz\n"
-    else:
-        cmd += "\n"
-    return cmd
-
-
-def get_edit_fastq_cmd(
-        reads: str,
-        num: int
-) -> str:
-    """Get the unix command to run on each fastq file that needs editing.
-
-    Parameters
-    ----------
-    reads : str
-    num : int
-        Can be 1 or 2 for the forward and reverse read, respectively.
-
-    Returns
-    -------
-    cmd : str
-        Unix command to run to edit a fastq file.
-    """
-    cmd = ''
-    fastq_fp = reads[num - 1]
-    line = get_fastq_header(fastq_fp)
-    if not line.endswith('/%s' % num):
-        line_split = line.split()
-        if len(line_split) > 1:
-            if line_split[1].startswith('%s:N:' % num):
-                cmd = edit_fastq_cmd(fastq_fp, num, 'illumina')
-            elif line_split[0].endswith('.%s' % num):
-                cmd = edit_fastq_cmd(fastq_fp, num, 'ebi')
-    return cmd
-
-
 def mkdr(path: str, is_file: bool = False) -> None:
     """Creates a folder is does not exist yet.
 
@@ -213,56 +93,48 @@ def mkdr(path: str, is_file: bool = False) -> None:
         os.makedirs(path, exist_ok=True)
 
 
-def get_pfam_file(
-        pfam_hmm: str
-) -> bool:
+def get_pfam_wget_cmd(pfam_dir: str) -> str:
     """Get the validation (even through downloading) that the HMM file is there.
 
     Parameters
     ----------
-    pfam_hmm : str
-        "Pfam-A.hmm" or "Pfam-A.hmm.dat""
+    pfam_dir : str
+        Path to the folder that should contain the "Pfam-A.*" files
 
     Returns
     -------
-    ret : bool
-        Whether the target file is finally available or not.
+    cmd : str
+        Command line to run to download the database
     """
-    ret = True
-    if not isfile(pfam_hmm):
-        user = input('"%s" not found...\nDownload? ([Y]/n)\n:' % pfam_hmm)
-        if not user or user.lower()[0] == 'y':
-            subprocess.call(
-                ('wget -O %s http://ftp.ebi.ac.uk/pub/databases/Pfam/releases'
-                 '/Pfam35.0/%s.gz' % (pfam_hmm, basename(pfam_hmm))).split())
-        else:
-            ret = False
-    return ret
+    cmd = ''
+    for ext in ['hmm', 'fasta', 'hmm.dat']:
+        fp = '%s/Pfam-A.%s' % (pfam_dir, ext)
+        if not isfile(fp):
+            gz = 'pub/databases/Pfam/releases/Pfam35.0/Pfam-A.%s.gz' % ext
+            cmd += 'wget -O %s http://ftp.ebi.ac.uk/%s\n' % (fp, gz)
+            cmd += 'gunzip %s\n' % fp
+    return cmd
 
 
-def get_hmm_dat(
-        dat: str,
-        tsv: str
-) -> pd.DataFrame:
+def get_hmm_dat(pfam_dir: str) -> pd.DataFrame:
     """Get as table the contents of
     http://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam35.0/Pfam-A.hmm.dat.gz
 
     Parameters
     ----------
-    dat : str
-        Path to Pfam dat (Pfam-A.hmm.dat.gz)
-    tsv : str
-        Path to reformatted, output Pfam dat (Pfam-A.hmm.dat.tsv)
+    pfam_dir : str
+        Path to the folder that should contain the "Pfam-A.*" files
 
     Returns
     -------
-    pfam_dat_pd : pd.DataFrame
+    pfam_pd : pd.DataFrame
         Pfam dat reformatted as table.
     """
+    tsv = '%s/Pfam-A.hmm.dat.tsv' % pfam_dir
     if not isfile(tsv):
         records = []
         cur = {}
-        with open(dat) as f:
+        with open('%s/Pfam-A.hmm.dat' % pfam_dir) as f:
             for line in f:
                 line_decode = line.strip()
                 if line_decode.startswith('//'):
@@ -270,19 +142,15 @@ def get_hmm_dat(
                     cur = {}
                 elif line_decode.startswith('#=GF'):
                     cur[line_decode[5:7]] = line_decode[7:].strip()
-        pfam_dat_pd = pd.DataFrame(records)
-        pfam_dat_pd.to_csv(tsv, index=False, sep='\t')
+        pfam_pd = pd.DataFrame(records)
+        pfam_pd.to_csv(tsv, index=False, sep='\t')
     else:
-        pfam_dat_pd = pd.read_table(tsv)
-    return pfam_dat_pd
+        pfam_pd = pd.read_table(tsv)
+    return pfam_pd
 
 
-def get_pfams_cmd(
-        hmm: str,
-        term_pd: pd.DataFrame,
-        pfam_dir: str,
-        term: str
-) -> tuple:
+def get_pfams_cmd(hmm: str, term_pd: pd.DataFrame,
+                  term: str, odir: str) -> tuple:
     """Collect the hmm profiles and diamond database for different term targets.
 
     Parameters
@@ -291,8 +159,8 @@ def get_pfams_cmd(
         Pfam-A.hmm file.
     term_pd : pd.DataFrame
         Data frame containing the Pfam-A.hmm.dat file, but reformatted above.
-    pfam_dir : str
-        Pfam database directory passed by the user.
+    odir : str
+        Directory where all Pfam models will be kept.
     term : str
         Term to search among the Pfam hmms.
 
@@ -306,7 +174,7 @@ def get_pfams_cmd(
     comp = re.compile("[\\ \-\",()%':&/.\[\]]")
     cmd = ''
     pfams = {}
-    pfam_out = '%s/%s' % (pfam_dir, term)
+    pfam_out = '%s/%s' % (odir, comp.sub('_', term))
     mkdr(pfam_out)
     for r, row in term_pd.iterrows():
         acc, desc = row['AC'], comp.sub('_', row['DE'])
@@ -314,12 +182,13 @@ def get_pfams_cmd(
         hmm_fp = '%s/%s.hmm' % (pfam_out, name)
         if not isfile(hmm_fp):
             cmd += 'hmmfetch %s %s >> %s\n' % (hmm, acc, hmm_fp)
-        fa = '%s/fastas/%s.fa' % (pfam_dir, acc)
-        fo = '%s/fastas/%s.fa' % (pfam_dir, name)
+        fa = '%s/fastas/%s.fa' % (odir, acc)
+        fo = '%s/%s.fa' % (pfam_out, name)
         dia = '%s/%s.dmnd' % (pfam_out, name)
         if not isfile(dia):
             cmd += 'cp %s %s\n' % (fa, fo)
             cmd += 'diamond makedb --in %s -d %s\n' % (fo, dia)
+            cmd += 'rm %s\n' % fo
         pfams[name] = [hmm_fp, dia]
     return pfams, cmd
 
@@ -344,46 +213,6 @@ def reads_lines(file_fp: str) -> list:
     return file_lines
 
 
-def count_reads_cmd(idx: int, input_path: str, out: str, sam: str) -> str:
-    """Get the command to count the reads in a fasta, fastq or fastq.gz file.
-
-    Parameters
-    ----------
-    idx : int
-        Unique, incemental numeric identifier.
-    input_path : str
-        Path to the input file name.
-    out : str
-        Path to the output file name.
-    sam: str
-        Sample name.
-
-    Returns
-    -------
-    cmd : str
-        Command to count reads and output the counts.
-    """
-    if input_path.endswith('fastq.gz'):
-        cmd = "n%s=`zcat %s | wc -l | " % (idx, input_path)
-        cmd += "sed 's/ //g' | awk '{x=$1/4; print x}'`\n"
-        # cmd += "cut -d ' ' -f 1 | awk '{x=$1/4; print x}'`"
-    elif input_path.endswith('fasta'):
-        cmd = "n%s=`wc -l %s | " % (idx, input_path)
-        cmd += "sed 's/ //g' | awk '{x=$1/2; print x}'`\n"
-        # cmd += "cut -d ' ' -f 1 | awk '{x=$1/2; print x}'`"
-    elif input_path.endswith('fastq'):
-        cmd = "n%s=`wc -l %s | " % (idx, input_path)
-        cmd += "sed 's/ //g' | awk '{x=$1/4; print x}'`\n"
-        # cmd += "cut -d ' ' -f 1 | awk '{x=$1/4; print x}'`"
-    else:
-        raise IOError("Input sequence file invalid %s" % input_path)
-    if idx:
-        cmd += 'echo "%s,2,${n%s}" >> %s\n' % (sam, idx, out)
-    else:
-        cmd += 'echo "%s,1,${n%s}" > %s\n' % (sam, idx, out)
-    return cmd
-
-
 def check_min_lines_count(input_fp: str) -> bool:
     """Check whether the number of lines in the file is above one.
 
@@ -406,11 +235,8 @@ def check_min_lines_count(input_fp: str) -> bool:
     return ret
 
 
-def get_out_dir(
-        out_dir: str,
-        inputs: dict,
-        sam_pool: str,
-        group: str = None) -> tuple:
+def get_out_dir_(out_dir: str, inputs: dict, sam_pool: str,
+                group: str = None) -> tuple:
     """Get the output directory for either the
 
     Parameters
@@ -443,16 +269,54 @@ def get_out_dir(
     return out_dir, file_path
 
 
-def write_hmms(out_dir: str, hmms_dias: dict) -> str:
+def get_out_dir(self, sam_pool, group: str = None) -> tuple:
+    """Get the output directory for either the
+
+    Parameters
+    ----------
+    self : Commands class instance
+        .dir : str
+            Path to pipeline output folder
+        .inputs : dict
+            Input files
+    sam_pool : str
+        Name of the current sample or pool.
+    group : str
+
+    Returns
+    -------
+    out_dir : str
+    file_path : str
+    """
+    out_dir = self.dir
+    inputs = self.inputs[sam_pool]
+    if group:
+        inputs = inputs[group]
+    if self.dir.endswith('after_plass'):
+        file_path = inputs.replace('nuclassembly', 'assembly')
+    elif self.dir.endswith('after_prodigal'):
+        file_path = inputs[1]
+        if 'after_spades' in file_path:
+            out_dir = out_dir.replace('after_', 'after_spades_')
+        elif 'after_plass' in file_path:
+            out_dir = out_dir.replace('after_', 'after_plass_')
+    else:
+        file_path = inputs[1]
+    out_dir = '%s/%s' % (out_dir, sam_pool)
+    return out_dir, file_path
+
+
+def write_hmms(self) -> str:
     """Write a file to contain on each line, the paths to the .hmm
     files to search as part of integron_finder.
 
     Parameters
     ----------
-    out_dir : str
-        Path to the output directory.
-    hmms_dias : dict
-        .hmm files per name of profile.
+    self : Commands class instance
+        .dir : str
+            Path to the output directory.
+        .databases.hmms_dias : dict
+            .hmm files per name of profile.
 
     Returns
     -------
@@ -460,11 +324,11 @@ def write_hmms(out_dir: str, hmms_dias: dict) -> str:
         Path to the file containing the paths to the .hmm files to search.
     """
     hmms_fp = ''
-    if hmms_dias:
-        mkdr(out_dir)
-        hmms_fp = '%s/hmm.txt' % out_dir
+    if self.databases.hmms_dias:
+        mkdr(self.dir)
+        hmms_fp = '%s/hmm.txt' % self.dir
         o = open(hmms_fp, 'w')
-        for target, gene_hmms in hmms_dias.items():
+        for target, gene_hmms in self.databases.hmms_dias.items():
             for gene, (hmm, _) in gene_hmms.items():
                 o.write('%s\n' % hmm)
         o.close()
@@ -479,48 +343,97 @@ def get_roundtrip(io) -> dict:
 def inputs_to_scratch(io) -> list:
     rsyncs, mkdirs = set(), set()
     # folders
-    for folder_ in io['I']['d']:
-        folder = folder_.rstrip('/')
-        src = folder_.rstrip('/').replace('${SCRATCH_FOLDER}', '')
-        mkdirs.add('mkdir -p %s' % folder)
-        rsyncs.add('rsync -aqruv %s/ %s' % (src, folder))
+    if ('I', 'd') in io:
+        for folder_ in io[('I', 'd')]:
+            folder = folder_.rstrip('/')
+            src = folder_.rstrip('/').replace('${SCRATCH_FOLDER}', '')
+            mkdirs.add('mkdir -p %s' % folder)
+            rsyncs.add('rsync -aqruv %s/ %s' % (src, folder))
     # files
-    for file in io['I']['f']:
-        folder = dirname(file)
-        src = file.replace('${SCRATCH_FOLDER}', '')
-        mkdirs.add('mkdir -p %s' % folder)
-        rsyncs.add('rsync -aqruv %s %s' % (src, file))
+    if ('I', 'f') in io:
+        for file in io[('I', 'f')]:
+            folder = dirname(file)
+            src = file.replace('${SCRATCH_FOLDER}', '')
+            mkdirs.add('mkdir -p %s' % folder)
+            rsyncs.add('rsync -aqruv %s %s' % (src, file))
     return sorted(mkdirs) + sorted(rsyncs)
 
 
 def outputs_back(io) -> list:
     outbound = set()
-    # folders
-    for folder_ in io['O']['d']:
-        folder = folder_.rstrip('/')
-        src = folder_.rstrip('/').replace('${SCRATCH_FOLDER}', '')
-        cmd = 'mkdir -p %s; rsync -aqruv %s/ %s' % (src, folder, src)
-        cmd = 'if [ -d %s ]; then %s' % (folder, cmd)
-        outbound.add(cmd)
-    # files
-    for file in io['O']['f']:
-        src = file.replace('${SCRATCH_FOLDER}', '')
-        folder = dirname(src)
-        cmd = 'mkdir -p %s; rsync -aqruv %s %s' % (folder, file, src)
-        cmd = 'if [ -f %s ]; then %s' % (file, cmd)
-        outbound.add(cmd)
+    if ('O', 'd') in io:
+        # folders
+        for folder_ in io[('O', 'd')]:
+            folder = folder_.rstrip('/')
+            src = folder_.rstrip('/').replace('${SCRATCH_FOLDER}', '')
+            cmd = 'mkdir -p %s; rsync -aqruv %s/ %s' % (src, folder, src)
+            cmd = 'if [ -d %s ]; then %s' % (folder, cmd)
+            outbound.add(cmd)
+    if ('O', 'f') in io:
+        # files
+        for file in io[('O', 'f')]:
+            src = file.replace('${SCRATCH_FOLDER}', '')
+            folder = dirname(src)
+            cmd = 'mkdir -p %s; rsync -aqruv %s %s' % (folder, file, src)
+            cmd = 'if [ -f %s ]; then %s' % (file, cmd)
+            outbound.add(cmd)
     return sorted(outbound)
 
 
-def scratching(soft) -> dict:
+def get_scratch_cmds(key, soft, cur_cmds, cmds):
+    if key in soft.io:
+        roundtrip = get_roundtrip(soft.io[key])
+        scratched_cmds = ['# Move to SCRATCH_FOLDER'] + roundtrip['to']
+        scratched_cmds += ['\n# %s commands (%s)' % (soft.name, key)] + cur_cmds
+        scratched_cmds += ['\n# Move from SCRATCH_FOLDER'] + roundtrip['from']
+        cmds[key] = scratched_cmds
+    else:
+        cmds[key] = cur_cmds
+
+
+def per_coassembly_scratch(pool, soft, sam_cmds, cmds, commands):
+    if pool in sam_cmds:
+        get_scratch_cmds(pool, soft, sam_cmds, cmds)
+    else:
+        for group in commands.pools[pool]:
+            group_cmds = sam_cmds[group]
+            get_scratch_cmds((pool, group), soft, group_cmds, cmds)
+
+
+def scratching(soft, commands) -> dict:
     if soft.params['scratch']:
         cmds = {}
+        # Use commands.pools to unpack the commands well
         for sam, sam_cmds in soft.cmds.items():
-            if sam in soft.io:
-                roundtrip = get_roundtrip(soft.io[sam])
-                cmds[sam] = (roundtrip['to'] + sam_cmds + roundtrip['from'])
+            if isinstance(sam_cmds, list):
+                get_scratch_cmds(sam, soft, sam_cmds, cmds)
+            elif isinstance(sam_cmds, dict):
+                per_coassembly_scratch(sam, soft, sam_cmds, cmds, commands)
             else:
-                cmds[sam] = soft.cmds[sam]
+                sys.exit('The collected commands are neither list of dict!')
         return cmds
     else:
         return soft.cmds
+
+
+def io_update(self, i_f=None, i_d=None, o_f=None, o_d=None, key=None):
+
+    for (IO_fd, val) in [
+        (('I', 'f'), i_f),
+        (('I', 'd'), i_d),
+        (('O', 'f'), o_f),
+        (('O', 'd'), o_d)
+    ]:
+        if not val:
+            continue
+        if isinstance(val, list):
+            if key:
+                self.outputs['io'][IO_fd].setdefault(key, set()).update(val)
+            else:
+                self.outputs['io'][IO_fd].update(val)
+        elif isinstance(val, str):
+            if key:
+                self.outputs['io'][IO_fd].setdefault(key, set()).add(val)
+            else:
+                self.outputs['io'][IO_fd].add(val)
+
