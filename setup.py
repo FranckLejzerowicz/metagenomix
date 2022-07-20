b69e1fd57bd718a8644e707e826c50b750134b58
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-07-20 12:32:43 +0200

    edit Xhpc version

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
         "pandas",
         "pyyaml",
         "seaborn",
-        "Xhpc==2.4"
+        "Xhpc==2.5"
     ],
     classifiers=classifiers,
     entry_points={'console_scripts': standalone},
