3a20aa06a54dbe9c7ec3fa6d07575393e2273f98
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-08-12 16:14:07 +0200

    update version

--- metagenomix/__init__.py
+++ metagenomix/__init__.py
@@ -6,4 +6,4 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-__version__ = "2.2.6"
+__version__ = "2.3.0"
