5ef52b2b7ebf3255a87c8a5cd8b50a2b7acc5de0
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-09-06 16:02:04 +0200

    remove superfluous command argument naming

--- metagenomix/scripts/modules/monitor.py
+++ metagenomix/scripts/modules/monitor.py
@@ -14,32 +14,32 @@ from metagenomix import __version__
 
 @click.command()
 @click.option(
-    "-i", "--i-fastq-illumina", multiple=True,
-    help="Path to short Illumina reads fastq files folder(s).")
+    "-i", "--fastq-illumina", multiple=True,
+    help="Path to short Illumina reads fastq files folder(s)")
 @click.option(
-    "-j", "--i-fastq-pacbio", multiple=True,
-    help="Path to long PacBio reads fastq files folder(s).")
+    "-j", "--fastq-pacbio", multiple=True,
+    help="Path to long PacBio reads fastq files folder(s)")
 @click.option(
-    "-k", "--i-fastq-nanopore", multiple=True,
-    help="Path to long MinION Nanopore reads fastq files folder.")
+    "-k", "--fastq-nanopore", multiple=True,
+    help="Path to long MinION Nanopore reads fastq files folder")
 @click.option(
-    "-m", "--m-metadata", required=True, help="Path to the metadata file.")
+    "-o", "--out-dir", required=True, help="Path to pipeline output folder")
 @click.option(
-    "-o", "--o-out-dir", required=True, help="Path to pipeline output folder.")
+    "-s", "--summary", help="Path to summary output file")
 @click.option(
-    "-s", "--o-summary", required=True, help="Path to summary output file.")
+    "-m", "--metadata", required=True, help="Path to the metadata file")
 @click.option(
-    "-d", "--p-databases", show_default=True, help="Databases (yaml file).")
+    "-d", "--databases", show_default=True, help="Databases (yaml file)")
 @click.option(
-    "-p", "--p-pipeline", show_default=True, help="Tools to pipeline.")
+    "-p", "--pipeline", show_default=True, help="Softwares to pipeline")
 @click.option(
-    "-u", "--p-run-params", show_default=True, help="Server run parameters.")
+    "-u", "--user-params", show_default=True, help="Server run parameters")
 @click.option(
-    "-c", "--p-co-assembly", show_default=True,
-    help="Metadata column(s) defining the co-assembly groups (yaml file).")
+    "-c", "--co-assembly", show_default=True,
+    help="Metadata column(s) defining the co-assembly groups (yaml file)")
 @click.option(
-    "-t", "--p-strains", show_default=True,
-    help="Species for strain-level analyses (yaml file).")
+    "-t", "--strains", show_default=True,
+    help="Species for strain-level analyses (yaml file)")
 @click.option(
     "--show-params/--no-show-params", default=False, show_default=False,
     help="Show all possible parameters for all softwares of your pipeline")
@@ -54,17 +54,17 @@ from metagenomix import __version__
     help="For development...")
 @click.version_option(__version__, prog_name="metagenomix")
 def monitor(
-        m_metadata,
-        i_fastq_illumina,
-        i_fastq_pacbio,
-        i_fastq_nanopore,
-        o_out_dir,
-        o_summary,
-        p_co_assembly,
-        p_databases,
-        p_pipeline,
-        p_run_params,
-        p_strains,
+        metadata,
+        fastq_illumina,
+        fastq_pacbio,
+        fastq_nanopore,
+        out_dir,
+        summary,
+        co_assembly,
+        databases,
+        pipeline,
+        user_params,
+        strains,
         show_params,
         force,
         verbose,
@@ -72,17 +72,17 @@ def monitor(
 ):
     """Check IO/job status of your pipeline configuration."""
     monitoring(
-        meta_fp=m_metadata,
-        illumina_dirs=i_fastq_illumina,
-        pacbio_dirs=i_fastq_pacbio,
-        nanopore_dirs=i_fastq_nanopore,
-        output_dir=o_out_dir,
-        summary_fp=o_summary,
-        coassembly_yml=p_co_assembly,
-        databases_yml=p_databases,
-        pipeline_tsv=p_pipeline,
-        user_params_yml=p_run_params,
-        strains_yml=p_strains,
+        meta_fp=metadata,
+        illumina_dirs=fastq_illumina,
+        pacbio_dirs=fastq_pacbio,
+        nanopore_dirs=fastq_nanopore,
+        output_dir=out_dir,
+        summary_fp=summary,
+        coassembly_yml=co_assembly,
+        databases_yml=databases,
+        pipeline_tsv=pipeline,
+        user_params_yml=user_params,
+        strains_yml=strains,
         show_params=show_params,
         force=force,
         verbose=verbose,
