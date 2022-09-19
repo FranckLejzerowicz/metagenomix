3fd4511fb5555eeb4b2dbe14210c040c8b1ea3c8
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-09-19 09:38:20 +0200

    fix prints

--- metagenomix/core/jobs.py
+++ metagenomix/core/jobs.py
@@ -355,10 +355,10 @@ class Created(object):
                 for link_ in links:
                     dest = link_.replace('${SCRATCH_FOLDER}', '')
                     src = self.commands.links[dest]
-                    o.write('m0=`md5sum %s | cut -d' ' -f 1`\n' % src)
+                    o.write("m0=`md5sum %s | cut -d' ' -f 1`\n" % src)
                     o.write('rm -rf %s\n' % dest)
                     o.write('cp -r %s %s\n' % (src, dest))
-                    o.write('m1=`md5sum %s | cut -d' ' -f 1`\n' % dest)
+                    o.write("m1=`md5sum %s | cut -d' ' -f 1`\n" % dest)
                     o.write('if [ "$m0" != "$m1" ]; then echo ')
                     o.write('"$m0 $m1 %s %s" >> %s; fi\n' % (src, dest, out))
                 o.write('echo "done"\n')
