27f5f8038a5643a5ba42f4138006969e1613399c
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-10-22 12:55:41 +0200

    add pipelines tools

---
+++ metagenomix/softwares/pipelines.py
@@ -0,0 +1,68 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2022, Franck Lejzerowicz.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import sys
+from os.path import dirname
+from metagenomix._inputs import (sample_inputs, group_inputs,
+                                 genome_key, genome_out_dir)
+from metagenomix._io_utils import io_update, to_do, status_update
+
+
+def squeezemeta(self) -> None:
+    """SqueezeMeta is a full automatic pipeline for
+    metagenomics/metatranscriptomics, covering all steps of the analysis.
+    SqueezeMeta includes multi-metagenome support allowing the co-assembly
+    of related metagenomes and the retrieval of individual genomes via
+    binning procedures. Thus, SqueezeMeta features several unique
+    characteristics.
+
+    References
+    ----------
+    Tamames, J. and Puente-Sánchez, F., 2019. SqueezeMeta, a highly portable,
+    fully automatic metagenomic analysis pipeline. Frontiers in microbiology,
+    9, p.3349.
+
+    Notes
+    -----
+    GitHub  : https://github.com/jtamames/SqueezeMeta
+    Paper   : https://doi.org/10.3389/fmicb.2018.03349
+    Docs    : https://github.com/jtamames/SqueezeMeta/wiki
+
+    Parameters
+    ----------
+    self
+    """
+    pass
+
+
+def anvio(self) -> None:
+    """Anvi’o is a comprehensive platform that brings together many aspects
+    of today’s cutting-edge computational strategies of data-enabled
+    microbiology, including genomics, metagenomics, metatranscriptomics,
+    pangenomics, metapangenomics, phylogenomics, and microbial population
+    genetics in an integrated and easy-to-use fashion through extensive
+    interactive visualization capabilities.
+
+    References
+    ----------
+    Eren, A.M., Kiefl, E., Shaiber, A., Veseli, I., Miller, S.E., Schechter,
+    M.S., Fink, I., Pan, J.N., Yousef, M., Fogarty, E.C. and Trigodet, F.,
+    2021. Community-led, integrated, reproducible multi-omics with anvi’o.
+    Nature microbiology, 6(1), pp.3-6.
+
+    Notes
+    -----
+    GitHub  : https://github.com/merenlab/anvio
+    Paper   : https://doi.org/10.1038/s41564-020-00834-3
+    Docs    : https://anvio.org/
+
+    Parameters
+    ----------
+    self
+    """
+    pass
