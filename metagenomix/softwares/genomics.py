93c8f9af4dcb16e0e8f36718faf3ccd39a1fa65b
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-11-17 17:55:15 +0100

    rename drep input to be unique bin fasta file names

--- metagenomix/softwares/genomics.py
+++ metagenomix/softwares/genomics.py
@@ -100,7 +100,8 @@ def get_bin_paths(
     """
     bin_paths_ = []
     for path in paths:
-        bin_paths_ = glob.glob('%s/*fa' % path.replace('${SCRATCH_FOLDER}', ''))
+        bin_paths_.extend(
+            glob.glob('%s/*fa' % path.replace('${SCRATCH_FOLDER}', '')))
         if self.config.dev:
             bin_paths_ = ['%s/a.fa' % path.replace('${SCRATCH_FOLDER}', ''),
                           '%s/b.fa' % path.replace('${SCRATCH_FOLDER}', '')]
@@ -268,7 +269,9 @@ def drep(self):
 
                 to_dos = status_update(
                     self, tech, paths, group=bin_algo, folder=True)
+                print(paths)
                 cmd_paths, cmd_rms, bin_paths = get_bin_paths(self, paths)
+                print(pathsdsa)
                 cmd_input, drep_in = get_drep_inputs(drep_out, bin_paths)
                 if not bin_paths:
                     self.soft.add_status(tech, pool, paths, group=bin_algo,
