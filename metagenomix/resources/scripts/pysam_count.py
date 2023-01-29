f9bdf538bc542695f9db9e7ec8e3e7f98565c606
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2023-01-29 21:07:19 +0100

    python3 pysam_count.py

--- metagenomix/resources/scripts/pysam_count.py
+++ metagenomix/resources/scripts/pysam_count.py
@@ -38,10 +38,30 @@ def assembling_counts(tab, filou):
     return dat
 
 
+def binning_counts(tab, filou):
+    pass
+    # dat = []
+    # for vals in tab:
+    #     alignment = pysam.AlignmentFile(vals[1], "rb")
+    #     with open(vals[0]) as f:
+    #         for line in f:
+    #             if line[0] == '>':
+    #                 contig = line[1:].split()[0]
+    #                 reads = alignment.count(contig)
+    #                 dat.append((vals[2:] + [contig, str(reads)]))
+    # with open(filou, 'w') as o:
+    #     o.write('tech\tsample\tali\ttarget\tprev\tmode\tcontig\treads\n')
+    #     for row in dat:
+    #         o.write('%s\n' % '\t'.join(row))
+    # return dat
+
+
 def count_reads(filin, filou):
-    tab = [x.strip() for x in open(filin).readlines()]
+    tab = [x.strip().split('\t') for x in open(filin).readlines()]
     if tab[-1][-1] == 'assembling':
         assembling_counts(tab, filou)
+    elif tab[-1][-1] == 'binning':
+        binning_counts(tab, filou)
 
 
 if __name__ == '__main__':
