c30c9b4deae9f8746761755ab523b4a3d2ea7f2d
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2023-02-04 13:38:39 +0100

    add pysam for prodigal / annotation

--- metagenomix/resources/scripts/pysam_count.py
+++ metagenomix/resources/scripts/pysam_count.py
@@ -21,7 +21,7 @@ def get_args():
     return args
 
 
-def assembling_counts(tab, filou):
+def assembling_counts(tab):
     dat = []
     for vdx, vals in enumerate(tab):
         if not vdx:
@@ -33,14 +33,27 @@ def assembling_counts(tab, filou):
                     contig = line[1:].split()[0]
                     reads = alignment.count(contig)
                     dat.append((vals[2:] + [contig, str(reads)]))
-    with open(filou, 'w') as o:
-        o.write('tech\tsample\tali\ttarget\tprev\tmode\tcontig\treads\n')
-        for row in dat:
-            o.write('%s\n' % '\t'.join(row))
     return dat
 
 
-def binning_counts(tab, filou):
+def prodigal_counts(tab):
+    dat = []
+    for vals in tab:
+        alignment = pysam.AlignmentFile(vals[1], "rb")
+        with open(vals[0]) as f:
+            for line in f:
+                if line[0] == '>':
+                    contig_split = line[1:].split()
+                    gene = contig_split[0]
+                    contig = gene.rsplit('_')[0]
+                    start = contig_split[2]
+                    end = contig_split[4]
+                    reads = alignment.count(contig, int(start), int(end))
+                    dat.append((vals[2:] + [gene, str(reads)]))
+    return dat
+
+
+def binning_counts(tab):
     pass
     # dat = []
     # for vals in tab:
@@ -58,14 +71,29 @@ def binning_counts(tab, filou):
     # return dat
 
 
+def write_output(dat, unit, filou):
+    head = 'tech\tsample\tali\tcoassembly\tgroup\t'
+    head += 'target\tprev\tmode\t%s\treads\n' % unit
+    with open(filou, 'w') as o:
+        o.write(head)
+        for row in dat:
+            o.write('%s\n' % '\t'.join(row))
+
+
 def count_reads(filin, filou):
     tab = [x.strip().split('\t') for x in open(filin).readlines()]
     if tab[-1][-1] == 'assembling':
-        assembling_counts(tab, filou)
+        unit = 'contig'
+        dat = assembling_counts(tab)
+    elif tab[-1][-1] == 'prodigal':
+        unit = 'gene'
+        dat = prodigal_counts(tab)
     elif tab[-1][-1] == 'binning':
-        binning_counts(tab, filou)
+        unit = 'bin'
+        dat = binning_counts(tab)
     else:
         print(skdfkjb)
+    write_output(dat, unit, filou)
 
 
 if __name__ == '__main__':
