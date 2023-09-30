29360a9670196e88a81db6a4b9a06020d715e364
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2023-09-30 20:31:11 +0200

    change new data structrure with hashes per soft

--- metagenomix/__init__.py
+++ metagenomix/__init__.py
@@ -1,9 +1,9 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2022, Franck Lejzerowicz.
+# Copyright (c) 2023, Franck Lejzerowicz.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-__version__ = "2.6.0"
+__version__ = "3.0"
