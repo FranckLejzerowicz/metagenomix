b5fbacd76724c902709f2704dd1c0897a878c86f
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2023-10-03 23:07:02 +0200

    add arg hamronization

--- metagenomix/softwares/args.py
+++ metagenomix/softwares/args.py
@@ -1562,7 +1562,10 @@ def get_hamronization(self, tech, inputs, group):
     self.outputs['dirs'].append(out_dir)
     self.outputs['outs'][(tech, group)] = out_dir
     cmd, cmd_rm, module, reports = get_arg_inputs(self, inputs)
-    to_dos = status_update(self, tech, [inputs], self.sam_pool, group=group)
+    if self.soft.prev in ['abricate']:
+        to_dos = status_update(self, tech, inputs, self.sam_pool, group=group)
+    else:
+        to_dos = status_update(self, tech, [inputs], self.sam_pool, group=group)
     key = genome_key(tech, group)
 
     out_fp = '%s/output.txt.gz' % out_dir
@@ -1573,7 +1576,7 @@ def get_hamronization(self, tech, inputs, group):
             cmd += hamronization_cmd(self, module, reports, out_dir)
             cmd += cmd_rm
             self.outputs['cmds'].setdefault(key, []).append(cmd)
-        io_update(self, i_f=inputs[0], o_d=out_dir, key=key)
+        io_update(self, i_f=inputs, o_d=out_dir, key=key)
         self.soft.add_status(tech, self.sam_pool, 1, group=group)
     else:
         self.soft.add_status(tech, self.sam_pool, 0, group=group)
