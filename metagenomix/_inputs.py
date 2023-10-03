6396946b88225b05af9c3eeed3919d4660724e21
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2023-10-04 00:53:12 +0200

    add arg hamronization

--- metagenomix/_inputs.py
+++ metagenomix/_inputs.py
@@ -827,7 +827,7 @@ def get_arg_inputs(self, inputs):
         module = 'deeparg'
         inp = "%s/nucleotide.sequences.mapping.ARG.gz" % inputs
         rep = splitext(inp)[0]
-        reports[rep] = ('%s.out' % module, '')
+        reports[rep] = ('%s.out' % module, 'nucleotide.sequences.mapping.ARG')
         cmd += "gunzip -c %s > %s\n" % (inp, rep)
         cmd_rm += "rm %s\n" % rep
     elif module == 'abricate':
