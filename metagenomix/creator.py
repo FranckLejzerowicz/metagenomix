a4cfe3f7c5018d43e4fed0335621b5381c574ef8
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2023-10-21 15:21:52 +0000

    use logger

--- metagenomix/creator.py
+++ metagenomix/creator.py
@@ -6,8 +6,21 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+import logging
 from metagenomix.metagenomix import metagenomix
 from metagenomix.core.jobs import Created
+
+
+def set_log():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(message)s')
+    # Using StreamHandler writing to console
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    # Add the two Handlers
+    logger.addHandler(ch)
 
 
 def creator(**kwargs):
@@ -18,20 +31,22 @@ def creator(**kwargs):
     kwargs : dict
         All arguments passed in command line, including defaults
     """
-    print('\n>>> `metagenomix create` started >>>\n')
+    set_log()
+    kwargs['logging'] = logging
+    logging.info('\n>>> `metagenomix create` started >>>\n')
     # Collect all command and init the script creating instance
     kwargs['command'] = 'create'
     creating = Created(*metagenomix(**kwargs))
     # print('* Writing database formatting commands')
     # creating.database_cmds()  # build the databases
-    print('* Creating output folders')
+    logging.info('* Creating output folders')
     creating.make_dirs()
-    print('* Writing pipeline command lines')
+    logging.info('* Writing pipeline command lines')
     creating.software_cmds()   # run the analysis pipeline
     if len(creating.run['database']) or len(creating.run['software']):
         m = '\n< PLEASE CONSIDER CHECKING THE COMMAND LINE SCRIPTS MANUALLY >'
-        print(m)
+        logging.info(m)
         creating.display()  # show the scripts to run
     creating.write_links()
     creating.write_logs()
-    print('\n<<< `metagenomix create` completed <<<\n')
+    logging.info('\n<<< `metagenomix create` completed <<<\n')
