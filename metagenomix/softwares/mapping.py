50bc944fbc0f34c450ed9ba3b8d89d1004ae4f92
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2024-08-09 13:20:51 +0200

    metadmg

--- metagenomix/softwares/mapping.py
+++ metagenomix/softwares/mapping.py
@@ -940,7 +940,7 @@ def metadmg_cmd(
     cmd += ' --names %s' % params['taxnames']
     cmd += ' --nodes %s' % params['taxnodes']
     cmd += ' --acc2tax %s' % params['acc2tax']
-    cmd += ' --metaDMG-cpp %s' % params['metaDMG_cpp']
+    cmd += ' --metaDMG-cpp %s' % params['path']
     cmd += ' --damage-mode %s' % params['damage_mode']
     cmd += ' --parallel-samples %s' % params['cpus']
     cmd += ' --sample-prefix %s' % group
