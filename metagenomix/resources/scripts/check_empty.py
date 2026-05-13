4de37b4183a121d0df5b7ba16a444dfffa444cd2
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2026-05-13 14:19:04 +0200

    add an empty file is all genomes are removed

--- metagenomix/resources/scripts/check_empty.py
+++ metagenomix/resources/scripts/check_empty.py
@@ -2,11 +2,15 @@ import click
 import pandas as pd
 
 @click.command()
-@click.option('-i',"--input", required=True, help='File for list of coverages.')
-@click.option('-e',"--empty", required=True, help="File to be written is no genome > min coverage")
-@click.option('-c', '--cutoff', default=0.1, help='Minimum % genome coverage.', show_default=True)
+@click.option('-i', "--input", required=True, help='File for list of coverages.')
+@click.option('-e', "--empty", required=True, help="File to be written is no genome > min coverage")
+@click.option('-c', "--cutoff", default=0.1, help='Minimum % genome coverage.', show_default=True)
 def check_empty(input, empty, cutoff):
     cov = pd.read_table(input, usecols=['gotu', 'coverage_ratio'])
+    print(cov['coverage_ratio'])
+    print(cutoff)
+    print((cov['coverage_ratio'] > cutoff))
+    print((cov['coverage_ratio'] > cutoff).sum())
     if (cov['coverage_ratio'] > cutoff).sum():
         with open(empty, 'w') as o:
             pass
